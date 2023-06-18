9df2fd42540c391e859b30df777c2586f9abbbc1
Isaac Wise <iamisaacwise2006@gmail.com>
2023-06-18 01:19:54 -0500

    Refactor fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,4 @@
-import dbConnect from '../lib/dbConnection';
-import Post from '../models/Post';
-import User from '../models/User';
-import { cookies } from 'next/headers';
-
-async function getPosts() {
-  dbConnect();
-  const user = await User.findOne({ refreshToken: cookies().get('refreshToken')?.value });
-  const userRole = user ? user.role : 'guest';
-
-  const publicPosts = await Post.find({ publicPost: true });
-  const privatePosts = await Post.find({ publicPost: false });
-
-  if (userRole === 'admin' || userRole === 'user') {
-    return [...publicPosts, ...privatePosts]
-  } else {
-    return [...publicPosts]
-  }
-}
+import getPosts from './requests/posts/getPosts';
 
 export default async function Home() {
   const posts = await getPosts();
--- src/app/requests/posts/getPosts.ts
+++ src/app/requests/posts/getPosts.ts
@@ -4,16 +4,8 @@ import http from '../http';
 const getPosts = async (): Promise<Post[]> => {
 	try {
 		const response = await http('/posts', { method: 'GET' });
-		const posts = response as Post[];
-
-		(async () => {
-			console.log(posts);
-		})();
-
-		return posts;
+		return response
 	} catch (error) {
-		// Handle error here
-		console.error('Error fetching posts:', error);
 		throw error;
 	}
 };
