039e3954cdb5ffb4088aa27d8fb5a92ad4546721
Isaac Wise <iamisaacwise2006@gmail.com>
2023-07-03 16:18:34 -0500

    Update packages & navbar stuff

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,12 @@
-'use client'
-import React, { useState } from 'react';
-import Button from './Button';
-import { MagnifyingGlassIcon } from '@heroicons/react/20/solid';
-import Modal from './Modal';
-import { login } from '../app/actions/auth';
+import React from 'react';
+import NavbarButtons from './NavbarButtons';
+import { getUserFromSession } from '../app/actions/user';
 
-// DONT USE ASYNC WITH CLIENT COMPONENTS
-function Navbar() {
-	const [open, setOpen] = useState(false);
-
-	const handleLogin = async () => {
-		const res = await login({ email: 'test@test.com', password: 'password' })
-		console.log(res)
-	}
+async function Navbar() {
+	const user = await getUserFromSession();
 
 	return (
 		<>
-			<Modal isOpen={open} setIsOpen={setOpen}>
-				<span className='text-white text-xl font-semibold'>Log In</span>
-			</Modal>
 			<nav className='flex-shrink-0 bg-[#1E2936] border-b border-[#4B5563]'>
 				<div className='mx-auto max-w-7xl px-4 sm:px-8'>
 					<div className='relative flex h-14 items-center justify-between'>
@@ -30,13 +18,7 @@ function Navbar() {
 								{breadcrumbs}
 							</div> */}
 						</div>
-						{/* When we write this search function just get the posts one time and filter through it, just match strings & filter with tags */}
-						<div className='flex flex-row gap-2'>
-							<div className='p-2 cursor-pointer flex items-center justify-center rounded-full bg-[#364150] hover:bg-[#465160] transition-all ease-linear'>
-								<MagnifyingGlassIcon className='h-5 w-5 text-gray-200' aria-hidden='true' />
-							</div>
-							<Button onClick={() => handleLogin()} variant={'gray'}>Login</Button>
-						</div>
+						<NavbarButtons user={user} />
 					</div>
 				</div>
 			</nav>
---
+++ src/components/NavbarButtons.tsx
@@ -0,0 +1,27 @@
+'use client'
+import React, { useState } from 'react';
+import Button from './Button';
+import { MagnifyingGlassIcon } from '@heroicons/react/20/solid';
+import Modal from './Modal';
+import { UserType } from '../app/types/User';
+
+function NavbarButtons({ user }: { user: UserType | null }) {
+	const [open, setOpen] = useState(false);
+
+	return (
+		<>
+			<Modal isOpen={open} setIsOpen={setOpen}>
+				<span className='text-white text-xl font-semibold'>Log In</span>
+			</Modal>
+			<div className='flex flex-row gap-2'>
+				<div className='p-2 cursor-pointer flex items-center justify-center rounded-full bg-[#364150] hover:bg-[#465160] transition-all ease-linear'>
+					<MagnifyingGlassIcon className='h-5 w-5 text-gray-200' aria-hidden='true' />
+				</div>
+				<Button onClick={() => setOpen(true)} variant={'gray'}>Login</Button>
+			</div>
+		</>
+	)
+};
+
+export default NavbarButtons;
+NavbarButtons.displayName = 'NavbarButtons';
