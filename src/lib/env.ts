6d838a852ee25c59445993f284f727ccb98aec51
Isaac Wise <iamisaacwise2006@gmail.com>
2023-08-13 16:52:45 -0500

    clean env differently

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,34 +1,9 @@
-import { z } from 'zod';
-import dotenv from 'dotenv';
+import { cleanEnv, str } from 'envalid'
 
-dotenv.config();
-
-const envSchema = z.object({
-  DB_URI: z.string().nonempty(),
-  JWT_SECRET: z.string().nonempty(),
-  APP_MODE: z.enum(['development', 'production']),
+const env = cleanEnv(process.env, {
+  DB_URI: str(),
+  JWT_SECRET: str(),
+  APP_MODE: str(),
 });
 
-const env = {
-  DB_URI: process.env.DB_URI,
-  JWT_SECRET: process.env.JWT_SECRET,
-  APP_MODE: process.env.APP_MODE,
-};
-
-const parsedEnv = envSchema.safeParse(env);
-
-if (!parsedEnv.success) {
-  throw new Error('No env found');
-}
-
-const validateEnv = async () => {
-  try {
-    parsedEnv.data;
-  } catch (err) {
-    throw new Error(`${err}`);
-  }
-};
-
-validateEnv();
-
-export default parsedEnv.data;
+export default env;
