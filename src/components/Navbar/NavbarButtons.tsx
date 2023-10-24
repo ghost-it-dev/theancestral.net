8fc8649ade78940f264919232f3d7468ea403128
Isaac Wise <iamisaacwise2006@gmail.com>
2023-10-23 23:27:05 -0500

    Remove search for now

--- src/components/Navbar/NavbarButtons.tsx
+++ src/components/Navbar/NavbarButtons.tsx
@@ -19,13 +19,13 @@ function NavbarButtons({ user }: { user: Omit<UserInterface, 'password'> | null 
         <span className='text-xl font-semibold text-white'>Search for a user or post</span>
       </Modal>
       <div className='flex flex-row gap-3'>
-        <div className='flex cursor-pointer items-center justify-center rounded-full bg-[#364150] p-2 transition-all ease-linear hover:bg-[#465160]'>
+        {/* <div className='flex cursor-pointer items-center justify-center rounded-full bg-[#364150] p-2 transition-all ease-linear hover:bg-[#465160]'>
           <MagnifyingGlassIcon
             onClick={() => setSearchOpen(!searchOpen)}
             className='h-5 w-5 text-gray-200'
             aria-hidden='true'
           />
-        </div>
+        </div> */}
         {user ? (
           <UserDropdown user={user} />
         ) : (
